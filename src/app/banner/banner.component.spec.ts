49cc075eaacf39a36ddcc8a19275923020882ce5
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2024-02-20 17:11:58 +0100

    add a unit test

--- src/app/banner/banner.component.spec.ts
+++ src/app/banner/banner.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BannerComponent } from './banner.component';
+import { Component } from '@angular/core';
 
 describe('BannerComponent', () => {
   let component: BannerComponent;
   let fixture: ComponentFixture<BannerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BannerComponent]
-    })
-    .compileComponents();
-    
+  beforeEach(() => {
     fixture = TestBed.createComponent(BannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit output when state changes', () => {
+    let expectedValue: undefined | boolean;
+    component.expandChange.subscribe(value => expectedValue = value);
+    
+    const toggleButton = fixture.nativeElement.querySelector('.info-toggle');
+
+    fixture.componentRef.setInput('expand', false);
+
+    toggleButton.click();
+    
+    expect(expectedValue).toBe(true);
   });
 });
+
+describe('BannerComponent | TestHost Strategy', () => {
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+
+  @Component({
+    standalone: true,
+    template: `
+      <app-banner [(expand)]="showMore" />
+    `,
+    imports: [BannerComponent]
+  })
+  class TestHostComponent {
+    showMore = false;
+  }
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should emit output when state changes', () => {
+
+    const toggleButton = fixture.nativeElement.querySelector('.info-toggle');
+    toggleButton.click();
+
+    expect(component.showMore).toBe(true);
+
+  })
+})
